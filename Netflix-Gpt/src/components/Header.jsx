c7392c474b45c92503623def6efddd21848b1e1a
hisamc91237 <hisamc2000@gmail.com>
2026-04-25 13:13:20 +0530

    Form validation started

--- Netflix-Gpt/src/components/Header.jsx
+++ Netflix-Gpt/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="absolute z-10 w-full px-8 py-2 bg-linear-to-b from-black flex flex-col md:flex-row justify-between">
+      <img
+        className="w-48"
+        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-04-24/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+    </div>
+  );
 };
 
 export default Header;
--- Netflix-Gpt/src/components/Login.jsx
+++ Netflix-Gpt/src/components/Login.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/ba53094c-3e3b-4789-94a6-baac10310f07/web/IN-en-20260420-TRIFECTA-perspective_52edec47-1b88-414a-bbbe-670f7229d886_large.jpg')] bg-cover bg-center h-screen">
+      <div className="absolute inset-0 bg-black/40"></div>
+      <Header />
+      <form
+        action=""
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-3"
+      >
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className=" text-white p-2 m-4"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className=" text-white p-2 m-4"
+        />
+        <button className="px-4 py-2 rounded-md bg-red-500 text-white">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
