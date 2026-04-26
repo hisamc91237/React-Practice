b83e4c6e6d15c2adab1f2802f5aa596807d80174
hisamc91237 <hisamc2000@gmail.com>
2026-04-27 00:44:54 +0530

    TMDB setup started

--- Netflix-Gpt/src/components/Browse.jsx
+++ Netflix-Gpt/src/components/Browse.jsx
@@ -1,6 +1,21 @@
 import HeaderBrowse from "./HeaderBrowse";
+import { API_OPTIONS } from "../utils/constants";
+import { useEffect } from "react";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS,
+    );
+    const json = await data.json();
+    console.log(json);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   return (
     <div>
       <HeaderBrowse />
